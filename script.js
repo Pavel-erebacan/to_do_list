1430c2dde554e9e292f8164929cbdf4b7d1d5852
Pavel <ppvr3407@gmail.com>
2021-01-11 22:16:54 +0200

    функционал кнопки сохранить

---
+++ script.js
@@ -0,0 +1,8 @@
+ function save() {
+     let x = document.getElementById("text_uzer").value;
+     var newDiv = document.createElement("p");
+     newDiv.className = 'notes';
+     newDiv.innerHTML = x;
+     my_div = document.getElementById("HREN");
+     document.body.insertBefore(newDiv, my_div);
+ }
