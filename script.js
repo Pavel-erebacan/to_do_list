30a7f2756ce7edf8c8079fb64dbe4e90c270fa95
Pavel <ppvr3407@gmail.com>
2021-01-31 21:30:30 +0200

    добавил изменения в если парс плюс парс отслежывает любые загрузки

--- script.js
+++ script.js
@@ -9,9 +9,6 @@
          alert("УЖЕ БЫЛО!");
          count--;
      } else {
-         localStorage.setItem("list", JSON.stringify(list));
-         list = JSON.parse(localStorage.getItem("list"));
-
          list.push(x.value);
          const r = [...container.children];
          let newDiv = document.createElement("p");
@@ -20,32 +17,35 @@
          newDiv.append();
          container.append(newDiv);
          x.value = "";
-         return parse;
-
-
-     }
- };
-
-
- function parse() {
-
-     list = JSON.parse(localStorage.getItem("list"));
-     for (let i = 0; i < list.length; i++) {
-         const container = document.querySelector('.container');
-         let x = document.getElementById('text_uzer');
-         const r = [...container.children];
-         let newDiv = document.createElement("p");
-         newDiv.className = 'notes';
-         newDiv.textContent = list[i];
-         newDiv.append();
-         container.append(newDiv);
-         x.value = "";
+         localStorage.setItem("list", JSON.stringify(list));
 
      }
  };
-
 
  function del() {
      localStorage.clear();
      location.reload();
+ };
+
+ document.addEventListener('DOMContentLoaded', parse);
+
+ function parse() {
+     if (localStorage.getItem("list") !== null) {
+         list = JSON.parse(localStorage.getItem("list"));
+         for (let i = 0; i < list.length; i++) {
+             const container = document.querySelector('.container');
+             let x = document.getElementById('text_uzer');
+             const r = [...container.children];
+             let newDiv = document.createElement("p");
+             newDiv.className = 'notes';
+             newDiv.textContent = list[i];
+             newDiv.append();
+             container.append(newDiv);
+             x.value = "";
+             count = -1 + (list.length);
+
+         }
+     }
+
+
  };
